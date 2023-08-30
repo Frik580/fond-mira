b9984e7ac959bd9069e11c8aa51dc1587a353cee
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-08-30 22:54:20 +0600

    feat: add projects logic

--- src/entities/Project/Project.tsx
+++ src/entities/Project/Project.tsx
@@ -2,12 +2,23 @@
 
 import "./Project.css";
 import Image from "next/image";
+import Link from "next/link";
 import imageProject from "./project.png";
 
 export const Project = () => {
     return (
-        <li onMouseEnter={() => console.log("Навел")} className="project">
-            <Image src={imageProject} className="project__image" alt="image" />
-        </li>
+        <Link href={`project`}>
+            <li onMouseEnter={() => console.log("Навел")} className="project">
+                <Image
+                    src={imageProject}
+                    className="project__image"
+                    alt="image"
+                />
+                <div className="project__conteiner">
+                    <p className="project__title">Пермь великая – ПАРМА многоликая</p>
+                    <p className="project__text">больше информации</p>
+                </div>
+            </li>
+        </Link>
     );
 };
