1ebe1641e9b9be2fbd6d40212d14e704df3cae2f
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2023-09-05 14:30:07 +0600

    feat: add Partners component

--- src/entities/Contacts/Contacts.tsx
+++ src/entities/Contacts/Contacts.tsx
@@ -1,16 +1,16 @@
 // "use client";
 
 import "./Contacts.css";
-import Image from "next/image";
-import imageNews from "./5_4T5J0z5xA.png";
-import { MainTitle } from "../MainTitle/MainTitle";
-import { TITLES } from "../../shared/Constants";
+// import Image from "next/image";
+// import imageNews from "./5_4T5J0z5xA.png";
+// import { MainTitle } from "../MainTitle/MainTitle";
+// import { TITLES } from "../../shared/Constants";
 
 export const Contacts = () => {
     return (
         <section className="contacts">
-            <MainTitle id="contacts" text={TITLES.CONTACTS} />
-            <div className="contacts__conteiner">
+            {/* <MainTitle id="contacts" text={TITLES.CONTACTS} /> */}
+            <div id="contacts" className="contacts__conteiner">
                <p>CONTACTS</p>
             </div>
         </section>
--- src/entities/Partners/Partners.tsx
+++ src/entities/Partners/Partners.tsx
@@ -1,8 +1,6 @@
 // "use client";
 
 import "./Partners.css";
-import Image from "next/image";
-import imageNews from "./5_4T5J0z5xA.png";
 import { MainTitle } from "../MainTitle/MainTitle";
 import { TITLES } from "../../shared/Constants";
 
@@ -10,9 +8,45 @@ export const Partners = () => {
     return (
         <section className="partners">
             <MainTitle id="partners" text={TITLES.PARTNERS} />
-            <div className="partners__conteiner">
-               <p>PARTNERS</p>
-            </div>
+            <ul className="partners__conteiner">
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Пермская региональная краеведческая общественная
+                        организация «Мы - земляки»
+                    </p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Пермская региональная общественная организация учителей
+                        «Ассоциация «Луч»
+                    </p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Ресурсный центр добровольчества Пермского края
+                    </p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Региональное отделение Российского военно- исторического
+                        общества
+                    </p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Оздоровительно-образовательный лагерь «Ребячий лагерь
+                        «Новое поколение»
+                    </p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">Фонд президентских грантов</p>
+                </li>
+                <li className="partners__item">
+                    <p className="partners__text">
+                        Президентский фонд культурных инициатив
+                    </p>
+                </li>
+            </ul>
         </section>
     );
 };
