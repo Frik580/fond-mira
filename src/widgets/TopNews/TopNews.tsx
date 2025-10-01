b5f34e69f9163d13c21c1fcdf20eb92df9569fad
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2025-10-01 21:44:25 +0500

    ref: top news

--- src/widgets/TopNews/TopNews.tsx
+++ src/widgets/TopNews/TopNews.tsx
@@ -10,21 +10,10 @@ export const TopNews = () => {
         <section className="top-news">
             <TopImage src={TOP_NEWS_COVER} srclite={TOP_NEWS_COVER_LITE} />
             <div className="top-news__conteiner">
-                <p className="top-news__title">Наше лето - 2025</p>
+                <p className="top-news__title">Наша осень - 2025</p>
                 <ul className="top-news__text">
                     <li>
-                        1 июля 2025 года - финиш Проекта &laquo;Рябиновые
-                        рассветы. Живая Память&raquo;, поддержанного Фондом
-                        президентских
-                    </li>
-                    <li>
-                        Полным ходом идет реализация проекта &laquo;Правнуки
-                        Победы. Живая Память&raquo; при поддержке Фонда Грантов
-                        губернатора Пермского края
-                    </li>
-                    <li>
-                        Краевой Медиа-Марафон &laquo;Виват, Победа!&raquo; продолжается!
-                        Приветствуем новых участников!
+                        Началась подготовка проекта &laquo;От Победы к Победе. Живая Память&raquo; на конкурс Фонда президентских грантов.
                     </li>
 
                     {/* <li>
