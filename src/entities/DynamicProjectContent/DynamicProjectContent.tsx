50d2762df3fc57cdf393f33b25ca7a92abd870fa
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2026-05-03 18:09:12 +0500

    feat: project text smooth visible

--- src/entities/DynamicProjectContent/DynamicProjectContent.tsx
+++ src/entities/DynamicProjectContent/DynamicProjectContent.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { ContentBlock } from "@/shared/models/Models";
 import renderTextWithLinks from "@/features/RenderTextWithLinks/RenderTextWithLinks";
+import { CSSProperties } from "react";
 
 interface Props {
     content?: ContentBlock[];
@@ -12,13 +13,17 @@ const renderItems = (
     items: (string | string[] | ContentBlock)[],
     listType?: "ul" | "ol",
     className?: string,
+    style?: CSSProperties,
 ): React.ReactNode => {
     const ListTag = listType === "ol" ? "ol" : "ul";
     const listClass =
         listType === "ol" ? "project-card__numlist" : "project-card__list";
 
     return (
-        <ListTag className={`${className || listClass} project-card__article`}>
+        <ListTag
+            className={`${className || listClass} project-card__article`}
+            style={style}
+        >
             {items.map((item, i) => (
                 <li key={i} className="li">
                     {Array.isArray(item) ? (
@@ -42,14 +47,17 @@ const renderItems = (
 
 const BlockRenderer = ({
     block,
+    index,
 }: {
     block: ContentBlock;
     index: number;
 }): React.ReactNode => {
+    const style = { "--i": index + 1 } as CSSProperties;
+
     switch (block.type) {
         case "paragraph":
             return (
-                <div className="text project-card__multi-text">
+                <div className="text project-card__multi-text" style={style}>
                     {block.boldText && (
                         <p className="text project-card__title">
                             {block.boldText}
@@ -65,7 +73,7 @@ const BlockRenderer = ({
 
         case "goal":
             return (
-                <p className="text">
+                <p className="text" style={style}>
                     <span className="text project-card__title">
                         {block.title}{" "}
                     </span>
@@ -78,11 +86,12 @@ const BlockRenderer = ({
                 block.items || [],
                 block.listType,
                 block.className,
+                style,
             );
 
         case "article":
             return (
-                <article className="project-card__article">
+                <article className="project-card__article" style={style}>
                     {block.title && (
                         <h2 className="text project-card__title">
                             {block.title}
@@ -109,7 +118,11 @@ const BlockRenderer = ({
             );
 
         case "document":
-            return <Document title={block.title} href={block.href} />;
+            return (
+                <div style={style}>
+                    <Document title={block.title} href={block.href} />
+                </div>
+            );
 
         case "image-link":
             return block.href && block.src ? (
@@ -118,6 +131,7 @@ const BlockRenderer = ({
                     href={block.href}
                     target="_blank"
                     rel="noopener noreferrer"
+                    style={style}
                 >
                     <Image
                         src={block.src}
@@ -134,10 +148,13 @@ const BlockRenderer = ({
 };
 
 export const DynamicProjectContent = ({ content }: Props) => {
+
     if (!content) return null;
 
     return (
-        <div className="project-card__conteiner">
+        <div
+            className="project-card__conteiner project-card__conteiner--visible"
+        >
             {content.map((block, index) => (
                 <BlockRenderer key={index} block={block} index={index} />
             ))}
