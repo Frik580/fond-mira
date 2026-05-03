238391bfbbd31911d4da5b5ee6b1ba23b027f8d3
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2026-05-03 13:31:51 +0500

    fix: remove top news bug

--- src/widgets/TopNews/TopNews.tsx
+++ src/widgets/TopNews/TopNews.tsx
@@ -13,7 +13,6 @@ export const TopNews = () => {
                 srcTablet={TOP_NEWS_COVER_TABLET} 
                 srcMobile={TOP_NEWS_COVER_MOBILE} 
                 srclite={TOP_NEWS_COVER_LITE} 
-                isInitialPage={true}
             />
         </section>
     );
